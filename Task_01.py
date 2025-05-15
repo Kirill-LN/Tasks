49ca0a15a23b900f8d4510d7a7e387c859770524
Kirill Laryshkin <larka69rus@gmail.com>
2025-05-15 23:46:26 +0300

    Удалил принты из методов

--- Task_01.py
+++ Task_01.py
@@ -1,15 +1,12 @@
 class Palidrome :
 
     def __new__(cls, *args, **kwargs):
-        print('Вызов __new__ для ' + str(cls) )
         return super().__new__(cls)
 
     def __init__(self, string):
-        print('Вызов метода __init__ для ' + str(self))
         self.string = string
 
     def isPalindrome(self):
-        print ('Вызов метода isPalindrome для ' + str(self))
         string = ''.join(filter(str.isalnum,self.string))
 
         if string.lower() == string[::-1].lower() :
