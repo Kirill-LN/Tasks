9da7da3a6c9e14d554230162cd42c8e9fd7a9d65
Kirill Laryshkin <larka69rus@gmail.com>
2025-05-15 20:01:27 +0300

    Исправление ошибки в задаче

--- Task_01.py
+++ Task_01.py
@@ -1,8 +1,24 @@
-string = input()
-def isPalindrome(string):
-    string =''.join(filter(str.isalnum, string))
-    return string.lower() == string[::-1].lower()
-print (isPalindrome(string))
+class String :
+
+    def __new__(cls, *args, **kwargs):
+        print('Вызов __new__ для ' + str(cls) )
+        return super().__new__(cls)
+
+    def __init__(self, string):
+        print('Вызов метода __init__ для ' + str(self))
+        self.string = string
+
+    def isPalindrome(self, string):
+        print ('Вызов метода isPalindrome для ' + str(self))
+        string = ''.join(filter(str.isalnum(string)))
+        return string.lower() == string[::-1].lower()
+
+
+string = String(input())
+
+string.isPalindrome(string)
+
+
 
 
 
