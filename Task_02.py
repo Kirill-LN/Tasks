6b2c243882d4240554312d31f33a25887b6f9b18
Kirill Laryshkin <larka69rus@gmail.com>
2025-08-12 20:32:38 +0300

    commit Task2

--- Task_02.py
+++ Task_02.py
@@ -1,21 +1,58 @@
+
+from numpy import number
+
 class ListFilter:
-
     def __init__(self):
-        self.input_list= list(map(int, input('Введите числа через пробел:').split()))
-        self.new_list = []
+        self.input_list = list( input('Введите числа через пробел:').split())
+        self.range_list = list(map(int, input('Введите диапозон:').split()))
 
-   # def - сделать проверку на тип данных
+        self.filter_list = []
+        self.results = []
 
-    def coincidence(self, range):
-        range_list = list(range)
-        for num in self.input_list:
-            if num in range_list:
-                self.new_list.append(num)
-        return self.new_list
+    def process_list(self):
+        if not self.input_list and not self.range_list:
+            return []
+        return self.filter_list
 
-filter = ListFilter()
-# сделать так что бы range работал через input
-filtered_list = filter.coincidence(range(input(map(int))))
-print('Новый список : ' , filtered_list)
+    def filter_input(self):
+            for item in self.input_list:
+                try:
+                    number = int(item)
+                    self.filter_list.append(number)
+                except ValueError:
+                    continue
+            return self.filter_list
+
+    def len_verify_range(self):
+        if len(self.range_list) != 2 :
+            print('Диапозон должен состоять из 2-х чисел')
+
+    def verify_type(self):
+
+        if not all(isinstance(number, int) for number in self.range_list):
+            raise TypeError('Все числа диапозона должны быть целыми')
 
 
+    def coincidence(self):
+        if not self.input_list or not self.range_list:
+            return []
+
+        self.filter_input()
+        self.len_verify_range()
+        self.verify_type()
+
+        self.coincidence_list = list(range(self.range_list[0], self.range_list[1]))
+
+        for number in self.filter_list:
+            if number in self.coincidence_list:
+                self.results.append(number)
+        return self.results
+
+
+list1 = ListFilter()
+
+try:
+    result = list1.coincidence()
+    print('Новый список :' , result)
+except (ValueError,TypeError) as e:
+    print('Ошибка : ' , e)
