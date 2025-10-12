2af8c07c2ea196f4eab17294820bc89520a75b58
Kirill <larka69rus@gmail.com>
2025-10-12 19:48:40 +0300

    Task_05 v2

--- Task_05.py
+++ Task_05.py
@@ -1,63 +1,44 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import timedelta
 
-class Data:
-    def __init__(self, data):
-        if isinstance(data, str):
-            self.data = data.split()
-        else:
-            self.data = data
+class Date:
+    def __init__(self):
+        """
+        Функция инициализируе нынешнюю дату
+        """
+        self.today = datetime.now()
 
-        self.future_data = None
-
-    def validate(self):
+    def getDate(self, format = '%d/%m/%Y %H:%M:%S'):
+        """
+        Возвращает текущую дату в заданном формате
+        По умолчанию: 'дд/мм/гггг ЧЧ:ММ:СС'
+        """
         try:
-            self.data = [int(x) for x in self.data]
-
-            if not (1 <= self.data[0] <= 31):
-                raise ('День должен быть от 1 до 31 ')
-            if not (1 <= self.data[1] <= 12):
-                raise ('Месяц должен быть от 1 до 12 ')
-            if not (0 <= self.data[3] <= 23):
-                raise ('Час должен быть от 1 до 23 ')
-            if not (0 <= self.data[4] <= 59):
-                raise ('Минуты должены быть от 0 до 59 ')
-            if not (0 <= self.data[4] <= 59):
-                raise ('Секунды должены быть от 0 до 59 ')
-        except ValueError:
-           print('Ошибка в формате ввода значений даты или времени')
-
-
-
-    def convertTime(self):
-        self.validate()
-
-        data_str = f'{self.data[0]:02d}-{self.data[1]:02d}-{self.data[2]}'
-        time_str = f'{self.data[3]:02d}:{self.data[4]:02d}:{self.data[5]:02d}'
-        full_date_str = f'{data_str} {time_str}'
-
-        self.data_obj = datetime.strptime(full_date_str, '%d-%m-%Y %H:%M:%S')
+            return self.today.strftime(format)
+        except:
+            return ('Ошибка при вводе данных')
 
     def date_in_future(self, integer):
-        try:
-            days= int(integer)
-            if hasattr(self, 'data_obj'):
-                self.future_data = self.data_obj + timedelta(days=days)
-            else:
-                print("Сначала вызовите convertTime()")
+        """
+        Принимает на ввод количество дней которые прибавляет к сегодняшней датеи времени
+        Выводит дату которая получилась послесложения
+        Если введено неверно число выводит сегодняшнюю дату и просит написать целое число исправив ошибку
+        """
+        try :
+            days = int(integer)
+            self.future_date = self.today + timedelta(days=days)
         except ValueError:
-            print("Введите корректное число дней")
+            print ('Введите корректное число дней')
 
     def output(self):
-
-        self.convertTime()
         try:
-            days_input = int(input('Введите количество дней: '))
-            self.date_in_future(days_input)
-            if self.future_data:
-                print(self.future_data.strftime('%d-%m-%Y %H:%M:%S'))
+            day_input= int(input('На сколько дней прибавим? :'))
+            self.date_in_future(day_input)
+            if self.future_date :
+                print (self.future_date.strftime('%d/%m/%Y %H:%M:%S'))
         except ValueError:
-            print(self.data_obj.strftime('%d-%m-%Y %H:%M:%S'))
-            print("Ошибка ввода. Пожалуйста, введите целое число.")
-            
-Today_data = Data(input('Какая сейчас дата и время ? Введите в формате: день, месяц, год, час, минуты, секунды (пример: 10 12 2025 13 54): '))
-Today_data.output()
+                print (self.today.strftime('%d/%m/%Y %H:%M:%S'))
+                print ('Введите целое число')
+
+today= Date()
+today.output()
